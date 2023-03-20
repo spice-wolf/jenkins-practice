5217a725c95d6c865c7b92abf1e1d5e2850e411f
pengwei <pengwei@xgd.com>
2023-03-20 14:08:30 +0800

    feat(controller,resources,test):新增一个便于测试的请求方法和单元测试方法

--- src/test/java/com/spice/JenkinsPracticeApplicationTests.java
+++ src/test/java/com/spice/JenkinsPracticeApplicationTests.java
@@ -1,13 +1,20 @@
 package com.spice;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+/**
+ * @author spice
+ */
 @SpringBootTest
 class JenkinsPracticeApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testJenkinsStage() {
+		System.out.println("执行了单元测试");
+		int expectedLife = 6;
+		int actualLife = 6;
+		Assertions.assertEquals(expectedLife, actualLife);
 	}
-
 }

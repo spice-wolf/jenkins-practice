0e3db4ddfac520d129c02960406a70a9ee5b2109
pengwei <pengwei@xgd.com>
2023-03-20 13:59:26 +0800

    Initial commit

---
+++ src/test/java/com/spice/JenkinsPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

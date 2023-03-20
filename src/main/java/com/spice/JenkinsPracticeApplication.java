eaac4ae83101cc1e8f6bee9c29123c88e3c9d54f
pengwei <pengwei@xgd.com>
2023-03-20 13:59:26 +0800

    Initial commit

---
+++ src/main/java/com/spice/JenkinsPracticeApplication.java
@@ -0,0 +1,15 @@
+package com.spice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author spice
+ */
+@SpringBootApplication
+public class JenkinsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsPracticeApplication.class, args);
+	}
+}
